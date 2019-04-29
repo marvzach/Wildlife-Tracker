cb9a2473e2f04a6a506761818a6819fc3253b697
Marvin Zachary <acerzach@gmail.com>
2019-04-29 21:25:27 +0300

    imports to animal.java and a constuctor to declare objects in the class

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,23 +1,23 @@
-// import org.sql2o.*;
-// import java.util.ArrayList;
-// import java.util.List;
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
 
-// public class Animal extends Animals {
+public class Animal extends Animals {
   
-//   public Animal(String name) {
-//     this.name = name;
-//     this.id = id;
-//   }
+  public Animal(String name) {
+    this.name = name;
+    this.id = id;
+  }
 
-//   @Override
-//   public boolean equals(Object otherAnimal) {
-//     if(!(otherAnimal instanceof Animal)) {
-//       return false;
-//     } else {
-//       Animal newAnimal = (Animal) otherAnimal;
-//       return this.getName().equals(newAnimal.getName());
-//     }
-//   }
+  @Override
+  public boolean equals(Object otherAnimal) {
+    if(!(otherAnimal instanceof Animal)) {
+      return false;
+    } else {
+      Animal newAnimal = (Animal) otherAnimal;
+      return this.getName().equals(newAnimal.getName());
+    }
+  }
 
 //   public void save() {
 //     try(Connection con = DB.sql2o.open()) {
