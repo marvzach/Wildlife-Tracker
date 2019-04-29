fb14c83c7ba3e8f9a73209494b47c54fce9f1399
Marvin Zachary <acerzach@gmail.com>
2019-04-29 21:35:14 +0300

    Added imports to animalTest.java and the first test

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,19 +1,19 @@
-// import org.junit.*;
-// import static org.junit.Assert.*;
-// import org.sql2o.*;
-// import java.util.ArrayList;
-// import java.util.List;
-// import java.util.Arrays;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
-// public class AnimalTest {
-//   @Rule
-//   public DatabaseRule database = new DatabaseRule();
+public class AnimalTest {
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
-//   @Test
-//   public void animal_instantiatesCorrectly_false() {
-//     Animal testAnimal = new Animal("Deer");
-//     assertEquals(true, testAnimal instanceof Animal);
-//   }
+  @Test
+  public void animal_instantiatesCorrectly_false() {
+    Animal testAnimal = new Animal("Deer");
+    assertEquals(true, testAnimal instanceof Animal);
+  }
 
 //   @Test
 //   public void getName_animalInstantiatesWithName_Deer() {
